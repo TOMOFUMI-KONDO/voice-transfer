73d8ddb35f1866cd15c630db569d35116fa08fbf
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-02-27 17:35:45 +0900

    受信側はタイムアウトしないようにした

--- src/voiceTransfer/receiver/VoiceReceiver.java
+++ src/voiceTransfer/receiver/VoiceReceiver.java
@@ -8,8 +8,6 @@ import java.net.DatagramSocket;
 public class VoiceReceiver {
     private static final int SERVER_PORT = 10007;
     private static final int PACKET_SIZE = (int) Math.pow(2, 14);
-    private static final int TIME_OUT_SECOND = 600;
-    private static final int WAIT = 100;
 
     public static void main(String[] args) {
         final byte[] buffer = new byte[PACKET_SIZE];
@@ -27,23 +25,9 @@ public class VoiceReceiver {
         try (final DatagramSocket socket = new DatagramSocket(SERVER_PORT)) {
             System.out.println("VoiceReceiverが起動しました。(port=" + socket.getLocalPort() + ")");
 
-            int count = 0;
             while (true) {
-                // fixme(kondo):
-                //  packetを受信しないとcountが増加しないようになっているので、packetの受信に関係なくcountを増加させる。
                 socket.receive(packet);
                 player.setVoice(buffer);
-
-                try {
-                    Thread.sleep(WAIT);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                count++;
-                if (count > (1000 / WAIT) * TIME_OUT_SECOND) {
-                    player.end();
-                }
 
                 if (!player.getIsPlaying()) {
                     break;
